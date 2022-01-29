f6043f56a42e71ce2e516905ba356249659fa1cb
Al-assad <yulin.ying@outlook.com>
2022-01-29 18:00:49 +0800

    modify SafeResult

--- potamoi-flink-gateway/src/main/scala/com/github/potamois/potamoi/flinkgateway/package.scala
+++ potamoi-flink-gateway/src/main/scala/com/github/potamois/potamoi/flinkgateway/package.scala
@@ -11,7 +11,7 @@ package object flinkgateway {
   case class Error(summary: String, errorStack: String)
 
   object SafeResult {
-    def pass[T](payload: Option[T]): SafeResult[T] = SafeResult(pass = true, None, payload)
+    def pass[T](payloadValue: T): SafeResult[T] = SafeResult(pass = true, None, Some(payloadValue))
 
     def fail[T](error: Error): SafeResult[T] = SafeResult(pass = false, Some(error), None)
 
