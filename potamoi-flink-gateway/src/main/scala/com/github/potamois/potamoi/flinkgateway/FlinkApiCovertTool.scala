d6b88dfe912c1d3b1700eab1b7ddebc5c1d9238a
Al-assad <yulin.ying@outlook.com>
2022-01-30 00:56:58 +0800

    use compatibility TableSchema API replace ResolvedSchema API

--- potamoi-flink-gateway/src/main/scala/com/github/potamois/potamoi/flinkgateway/FlinkApiCovertTool.scala
+++ potamoi-flink-gateway/src/main/scala/com/github/potamois/potamoi/flinkgateway/FlinkApiCovertTool.scala
@@ -1,6 +1,6 @@
 package com.github.potamois.potamoi.flinkgateway
 
-import org.apache.flink.table.catalog.ResolvedSchema
+import org.apache.flink.table.api.TableSchema
 import org.apache.flink.types.Row
 
 import scala.collection.JavaConverters._
@@ -8,19 +8,31 @@ import scala.collection.JavaConverters._
 /**
  * @author Al-assad
  */
+
 object FlinkApiCovertTool {
 
-
   /**
-   * Convert Flink [[ResolvedSchema]] to Potamoi [[Column]] sequence.
+   * Covert Flink [[TableSchema]] to Potamoi [[TableSchema]].
+   * Since ResolvedSchema is only available since flink-1.13, to maintain compatibility with the
+   * deprecated TableSchema API.
+   * Please replace it with ResolvedSchema when the minimum flink version supported by potamoi
+   * is from 1.13.
    *
    * @param schema should not be null
    */
-  def covertResolvedSchema(schema: ResolvedSchema): Seq[Column] = {
+  // noinspection ScalaDeprecation
+  def covertTableSchema(schema: TableSchema): Seq[Column] = {
+    schema.getTableColumns.asScala.map(col =>
+      if (col == null) Column("", "")
+      else Column(col.getName, col.getType.toString)
+    )
+  }
+
+  /*  def covertResolvedSchema(schema: ResolvedSchema): Seq[Column] = {
     schema.getColumns.asScala.map(col =>
       if (col == null) Column("", "")
       else Column(col.getName, col.getDataType.toString))
-  }
+  }*/
 
   /**
    * Covert Flink [[Row]] to Potamoi [[RowData]].
