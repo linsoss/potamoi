0384343acc7bcee18e53a3183662e23769c06a16
Al-assad <yulin.ying@outlook.com>
2022-01-30 12:43:24 +0800

    update UsingSpec

--- potamoi-commons/src/test/scala/com/github/potamois/potamoi/commons/UsingSpec.scala
+++ potamoi-commons/src/test/scala/com/github/potamois/potamoi/commons/UsingSpec.scala
@@ -11,12 +11,17 @@ class UsingSpec extends AnyWordSpec with Matchers {
     var isClosed = false
     var isConsumed = false
     def consume(): Unit = isConsumed = true
+    def offer(): Int = 1
     @throws[Exception] def forceThrowException(): Unit = throw new Exception("handling resource failures")
     override def close(): Unit = isClosed = true
   }
 
   object Resource {
     @throws[Exception] def failCreate(): Resource = throw new Exception("resource creation failed")
+  }
+
+  class CloseFailResource extends AutoCloseable {
+    @throws[Exception] def close(): Unit = throw new Exception("close resource failed")
   }
 
   "Using" should {
@@ -44,7 +49,9 @@ class UsingSpec extends AnyWordSpec with Matchers {
     }
 
     "error interception" in {
-      assertThrows[Exception] {new Resource().forceThrowException()}
+      assertThrows[Exception] {
+        new Resource().forceThrowException()
+      }
       val resource = new Resource
       val re = Using(resource) { rs =>
         rs.consume()
@@ -74,11 +81,46 @@ class UsingSpec extends AnyWordSpec with Matchers {
     }
 
     "fail to create resource" in {
-      assertThrows[Exception] {Resource.failCreate()}
+      assertThrows[Exception] {
+        Resource.failCreate()
+      }
       val re = Using(Resource.failCreate()) { rs => rs.consume() }
       re.isFailure shouldBe true
       re.failed.get.getMessage shouldBe "resource creation failed"
     }
+
+    "fail to release resource" in {
+      val resource = new CloseFailResource
+      val re = Using(resource) { rs => }
+      re.isFailure shouldBe true
+      re.failed.get.getMessage shouldBe "close resource failed"
+    }
+
+    "fail to release multiple resource" in {
+      val resource1 = new CloseFailResource
+      val resource2 = new Resource
+      val re = Using.Manager { use =>
+        val rs1 = use(resource1)
+        val rs2 = use(resource2)
+        rs2.consume()
+      }
+      re.isFailure shouldBe true
+    }
+
+    "Using.resource" in {
+      Using.resource(new Resource) { rs => rs.offer() } shouldBe 1
+      Using.resources(new Resource, new Resource) { case (rs1, rs2) =>
+        rs1.offer() + rs2.offer()
+      } shouldBe 2
+      Using.resources(new Resource, new Resource, new Resource) { case (rs1, rs2, rs3) =>
+        rs1.offer() + rs2.offer() + rs3.offer()
+      } shouldBe 3
+      Using.resources(new Resource, new Resource, new Resource, new Resource) { case (rs1, rs2, rs3, rs4) =>
+        rs1.offer() + rs2.offer() + rs3.offer() + rs4.offer()
+      } shouldBe 4
+
+    }
+
   }
 
 }
