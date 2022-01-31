394bb463a51ff7f518b58c7ae708297120698554
Al-assad <yulin.ying@outlook.com>
2022-01-31 15:53:24 +0800

    update uuid tools

--- potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/Uuid.scala
+++ potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/Uuid.scala
@@ -10,12 +10,16 @@ object Uuid {
   /**
    * Generate a uuid of length 36
    */
-  def genUUID: String = java.util.UUID.randomUUID().toString
+  def genUUID36: String = java.util.UUID.randomUUID().toString
 
   /**
    * Generate a uuid of length 32
    */
-  def genShortUUID: String = genUUID.split("-").mkString
+  def genUUID32: String = genUUID36.split("-").mkString
 
+  /**
+   * Generate a uuid of length 16
+   */
+  def genUUID16: String = genUUID32.substring(0, 16)
 
 }
--- potamoi-commons/src/test/scala/com/github/potamois/potamoi/commons/UuidSpec.scala
+++ potamoi-commons/src/test/scala/com/github/potamois/potamoi/commons/UuidSpec.scala
@@ -7,18 +7,33 @@ class UuidSpec extends AnyWordSpec with Matchers {
 
   "Uuid" should {
 
-    "genUUID" in {
-      val uuid = Uuid.genUUID
+    "genUUID36" in {
+      val uuid = Uuid.genUUID36
       uuid.length shouldBe 36
     }
 
-    "genShortUUID" in {
-      val uuid = Uuid.genShortUUID
+    "genUUID32" in {
+      val uuid = Uuid.genUUID32
       uuid.length shouldBe 32
     }
 
-    "uuids cannot be repeated" in {
-      val uuids = (1 to 1000).map(_ => Uuid.genShortUUID)
+    "genUUID16" in {
+      val uuid = Uuid.genUUID16
+      uuid.length shouldBe 16
+    }
+
+    "36 bytes uuids should not be repeated" in {
+      val uuids = (1 to 1000).map(_ => Uuid.genUUID36)
+      uuids.distinct.length shouldBe uuids.length
+    }
+
+    "32 bytes uuids should not be repeated" in {
+      val uuids = (1 to 1000).map(_ => Uuid.genUUID32)
+      uuids.distinct.length shouldBe uuids.length
+    }
+
+    "16 bytes uuids should not be repeated" in {
+      val uuids = (1 to 1000).map(_ => Uuid.genUUID16)
       uuids.distinct.length shouldBe uuids.length
     }
 
