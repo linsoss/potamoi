b420beb5516ac72d67b38a38a21787645bcfe205
assad <assad.dev@outlook.com>
2022-03-11 14:33:10 +0800

    add Error with timestamp mark

--- potamoi-flink-gateway/src/main/scala/com/github/potamois/potamoi/gateway/flink/Error.scala
+++ potamoi-flink-gateway/src/main/scala/com/github/potamois/potamoi/gateway/flink/Error.scala
@@ -1,5 +1,7 @@
 package com.github.potamois.potamoi.gateway.flink
 
+import com.github.potamois.potamoi.commons.{CborSerializable, curTs}
+
 /**
  * Error information.
  *
@@ -7,4 +9,17 @@ package com.github.potamois.potamoi.gateway.flink
  * @param stack   exception stack
  * @author Al-assad
  */
-case class Error(summary: String, stack: Throwable)
+case class Error(summary: String, stack: Throwable) extends CborSerializable {
+  def toTsError(ts: Long = curTs): TsError = TsError(summary, stack, ts)
+}
+
+/**
+ * Error information with timestamp marked.
+ *
+ * @param summary summary message
+ * @param stack   exception stack
+ * @param ts      error timestamp marked
+ */
+case class TsError(summary: String, stack: Throwable, ts: Long) extends CborSerializable {
+  def toError: Error = Error(summary, stack)
+}
