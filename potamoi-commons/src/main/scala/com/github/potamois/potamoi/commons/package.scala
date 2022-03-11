cd260a830b6b0c208879c750752ed7e2f599ead7
assad <assad.dev@outlook.com>
2022-03-11 13:45:57 +0800

    remove current timestamp function

--- potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/package.scala
+++ potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/package.scala
@@ -2,10 +2,6 @@ package com.github.potamois.potamoi
 
 package object commons {
 
-  /**
-   * Get current timestamp from system.
-   */
-  def currentTimestamp: Long = System.currentTimeMillis
 
   /**
    * Get current timestamp from system.
