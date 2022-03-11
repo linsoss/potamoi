31be2044c4d80452b83583b404783f91b91b4cac
assad <assad.dev@outlook.com>
2022-03-11 12:07:40 +0800

    current timestamp shortcut

---
+++ potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/package.scala
@@ -0,0 +1,17 @@
+package com.github.potamois.potamoi
+
+package object commons {
+
+  /**
+   * Get current timestamp from system.
+   */
+  def currentTimestamp: Long = System.currentTimeMillis
+
+  /**
+   * Get current timestamp from system.
+   */
+  def curTs: Long = System.currentTimeMillis
+
+
+
+}
