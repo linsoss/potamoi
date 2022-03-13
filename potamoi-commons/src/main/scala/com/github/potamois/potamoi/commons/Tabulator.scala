990465ad781c73ad6d7f8691bcd0f3dfab3c7b89
assad <assad.dev@outlook.com>
2022-03-13 23:13:29 +0800

    update

--- potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/Tabulator.scala
+++ potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/Tabulator.scala
@@ -18,30 +18,28 @@ object Tabulator {
    * @param table table content
    * @return Formatting tabulation string
    */
-  def format(table: Seq[Seq[Any]]): String =
-    Try(table match {
-      case Nil => ""
-      case _ =>
-        val cellSizes = table.map {
-          _.map { c =>
-            Option(c).fold(0) {
-              case s: String => s.length
-              case a => a.toString.length
-            } + CellPadding
-          }
+  def format(table: Seq[Seq[Any]]): String = Try(table match {
+    case Nil => ""
+    case _ =>
+      val cellSizes = table.map {
+        _.map { c =>
+          Option(c).fold(0) {
+            case s: String => s.length
+            case a => a.toString.length
+          } + CellPadding
         }
-        val colSizes = cellSizes.transpose.map(_.max)
-        val rows = table.map(r => formatRow(r, colSizes))
-        formatRows(rowSeparator(colSizes), rows)
-    }).getOrElse("")
+      }
+      val colSizes = cellSizes.transpose.map(_.max)
+      val rows = table.map(r => formatRow(r, colSizes))
+      formatRows(rowSeparator(colSizes), rows)
+  }).getOrElse("")
 
-  private def formatRow(row: Seq[Any], colSizes: Seq[Int]) =
-    row.zip(colSizes)
-      .map {
-        case (_, size: Int) if size == 0 => ""
-        case (item, size) =>
-          (" %-" + (size - CellPadding) + "s ").format(item)
-      }.mkString("|", "|", "|")
+  private def formatRow(row: Seq[Any], colSizes: Seq[Int]) = row.zip(colSizes)
+    .map {
+      case (_, size: Int) if size == 0 => ""
+      case (item, size) =>
+        (" %-" + (size - CellPadding) + "s ").format(item)
+    }.mkString("|", "|", "|")
 
   private def formatRows(separator: String, rows: Seq[String]) =
     (separator :: rows.head :: separator :: rows.tail.toList ::: separator :: Nil).mkString(NL)
