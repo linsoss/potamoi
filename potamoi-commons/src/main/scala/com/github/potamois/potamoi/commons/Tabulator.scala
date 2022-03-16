8cc18cf2499a71a950b35b9fe98d48c8a0dcee3f
assad <assad.dev@outlook.com>
2022-03-17 01:26:50 +0800

    Optimize Tabulator

--- potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/Tabulator.scala
+++ potamoi-commons/src/main/scala/com/github/potamois/potamoi/commons/Tabulator.scala
@@ -1,5 +1,8 @@
 package com.github.potamois.potamoi.commons
 
+import org.apache.commons.text.StringEscapeUtils
+
+import scala.collection.mutable
 import scala.util.Try
 
 /**
@@ -15,13 +18,15 @@ object Tabulator {
   /**
    * Generate an ASCII table for a given data set
    *
-   * @param table table content
+   * @param table      table content
+   * @param escapeJava escapes the characters in a String using Java String rules.
    * @return Formatting tabulation string
    */
-  def format(table: Seq[Seq[Any]]): String = Try(table match {
+  def format(table: Seq[Seq[Any]], escapeJava: Boolean = true): String = table match {
     case Nil => ""
-    case _ =>
-      val cellSizes = table.map {
+    case _ => Try {
+      val cleanTable = covertCellToString(table, escapeJava)
+      val cellSizes = cleanTable.map {
         _.map { c =>
           Option(c).fold(0) {
             case s: String => s.length
@@ -30,16 +35,47 @@ object Tabulator {
         }
       }
       val colSizes = cellSizes.transpose.map(_.max)
-      val rows = table.map(r => formatRow(r, colSizes))
+      val rows = cleanTable.map(r => formatRow(r, colSizes))
       formatRows(rowSeparator(colSizes), rows)
-  }).getOrElse("")
+    }.getOrElse("")
+  }
 
-  private def formatRow(row: Seq[Any], colSizes: Seq[Int]) = row.zip(colSizes)
-    .map {
-      case (_, size: Int) if size == 0 => ""
-      case (item, size) =>
-        (" %-" + (size - CellPadding) + "s ").format(item)
-    }.mkString("|", "|", "|")
+  private def covertCellToString(table: Seq[Seq[Any]], escapeJava: Boolean): mutable.Buffer[Seq[String]] =
+    if (escapeJava) {
+      table.map(_.map {
+        case null => ""
+        case s: String => StringEscapeUtils.escapeJava(s)
+        case o => o.toString
+      }).toBuffer
+    } else {
+      val unfoldTable = mutable.Buffer[Seq[String]]()
+      // handle cell that contains multiple lines
+      for (line <- table) {
+        val foldCells = line.zipWithIndex.filter(e => e._1 != null && e._1.toString.contains(NL)).map(e => e._2 -> e._1.toString.split(NL))
+        if (foldCells.isEmpty) {
+          unfoldTable += line.map(cell => if (cell == null) "" else cell.toString)
+        } else {
+          val maxDepth = foldCells.map(_._2.length).max
+          val foldCellsMap = foldCells.toMap
+          val matrix = (0 until maxDepth).map(_ => Array.fill(line.length)("").toBuffer)
+          // fill the first line of matrix
+          for (y <- line.indices)
+            if (!foldCellsMap.contains(y)) matrix.head(y) = if (line(y) == null) "" else line(y).toString
+          // fill the unfold cells into the matrix
+          for (y <- foldCellsMap.keys)
+            for (x <- foldCellsMap(y).indices)
+              matrix(x)(y) = foldCellsMap(y)(x)
+          unfoldTable ++= matrix
+        }
+      }
+      unfoldTable
+    }
+
+  private def formatRow(row: Seq[Any], colSizes: Seq[Int]): String = row.zip(colSizes).map {
+    case (_, size: Int) if size == 0 => ""
+    case (item, size) =>
+      (" %-" + (size - CellPadding) + "s ").format(item)
+  }.mkString("|", "|", "|")
 
   private def formatRows(separator: String, rows: Seq[String]) =
     (separator :: rows.head :: separator :: rows.tail.toList ::: separator :: Nil).mkString(NL)
--- potamoi-commons/src/test/scala/com/github/potamois/potamoi/commons/TabulatorSpec.scala
+++ potamoi-commons/src/test/scala/com/github/potamois/potamoi/commons/TabulatorSpec.scala
@@ -8,6 +8,16 @@ class TabulatorSpec extends STSpec {
 
   "Tabulator" should {
 
+    "format a tablbe 2" in {
+      val table = Seq(
+        Seq("f1", "f2", "f3", "f4", "f5", "f6"),
+        Seq("d", 302.087, 133L, 2333, true, "hello world"),
+        Seq("g", 3.231, 233L, 3342, false, "deep dark fantasy"),
+      )
+      val tableStr = Tabulator.format(table)
+      println(tableStr)
+    }
+
     "format a table" in {
       val table = Seq(
         Seq("f1", "f2", "f3", "f4", "f5", "f6"),
@@ -26,6 +36,7 @@ class TabulatorSpec extends STSpec {
       tableStr shouldBe expected
     }
 
+
     "format a table contains null values" in {
       val table = Seq(
         Seq("f1", "f2", "f3"),
@@ -39,9 +50,9 @@ class TabulatorSpec extends STSpec {
         """+------+--------+---------+
           || f1   | f2     | f3      |
           |+------+--------+---------+
-          || deep | 114514 | null    |
-          || dark | null   | null    |
-          || null | null   | fantasy |
+          || deep | 114514 |         |
+          || dark |        | null    |
+          ||      |        | fantasy |
           |+------+--------+---------+""".stripMargin
       tableStr shouldBe expected
     }
@@ -55,6 +66,42 @@ class TabulatorSpec extends STSpec {
     "format a table that rows of varying length" in {
       Tabulator.format(Seq(Seq("a", "b", "c"), Seq("d", "e", "f"), Seq("g", "h", "i", "233"))) shouldBe ""
       Tabulator.format(Seq(Seq("a", "b", "c"), Seq.empty)) shouldBe ""
+    }
+
+    "format a table that contains cell with multiple line" in {
+      val table = Seq(
+        Seq("f1", "f2", "f3"),
+        Seq(1, "line1", "line2"),
+        Seq(2,
+          """bala
+            |balabalabalabala
+            |balabala
+            |""".stripMargin,
+          """balabala
+            |balabala
+            |bala
+            |balabalabalabalabalabalabalabalas""".stripMargin),
+        Seq(3, "line3", "line4")
+      )
+      Tabulator.format(table, escapeJava = false) shouldBe
+      """+----+------------------+-----------------------------------+
+        || f1 | f2               | f3                                |
+        |+----+------------------+-----------------------------------+
+        || 1  | line1            | line2                             |
+        || 2  | bala             | balabala                          |
+        ||    | balabalabalabala | balabala                          |
+        ||    | balabala         | bala                              |
+        ||    |                  | balabalabalabalabalabalabalabalas |
+        || 3  | line3            | line4                             |
+        |+----+------------------+-----------------------------------+""".stripMargin
+      Tabulator.format(table, escapeJava = true) shouldBe
+      """+----+------------------------------------+-------------------------------------------------------------+
+        || f1 | f2                                 | f3                                                          |
+        |+----+------------------------------------+-------------------------------------------------------------+
+        || 1  | line1                              | line2                                                       |
+        || 2  | bala\nbalabalabalabala\nbalabala\n | balabala\nbalabala\nbala\nbalabalabalabalabalabalabalabalas |
+        || 3  | line3                              | line4                                                       |
+        |+----+------------------------------------+-------------------------------------------------------------+""".stripMargin
     }
   }
 
