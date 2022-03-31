99e0eb7f47708bd66617f261444e78acc83db6b5
assad <assad.dev@outlook.com>
2022-03-31 16:11:44 +0800

    Change the default akka config that test case read.

--- potamoi-akka-toolkit/src/test/scala/com/github/potamois/potamoi/akka/testkit/package.scala
+++ potamoi-akka-toolkit/src/test/scala/com/github/potamois/potamoi/akka/testkit/package.scala
@@ -5,6 +5,6 @@ import com.typesafe.config.{Config, ConfigFactory}
 package object testkit {
 
   // default akka config for scalatest
-  val defaultConfig: Config = ConfigFactory.load("application-test")
+  val defaultConfig: Config = ConfigFactory.load("application")
 
 }
