621c3cb9567a7a56140219a42ac6b58be8c0150a
assad <assad.dev@outlook.com>
2022-04-03 13:02:03 +0800

    Optimize cluster mock test tool to allow current system to join the cluster

--- potamoi-akka-toolkit/src/test/scala/com/github/potamois/potamoi/akka/testkit/STAkkaClusterMockSpec.scala
+++ potamoi-akka-toolkit/src/test/scala/com/github/potamois/potamoi/akka/testkit/STAkkaClusterMockSpec.scala
@@ -1,7 +1,8 @@
 package com.github.potamois.potamoi.akka.testkit
 
 import akka.actor.typed.{ActorSystem, Behavior}
-import akka.cluster.{Cluster, MemberStatus}
+import akka.cluster.MemberStatus
+import akka.cluster.typed.{Cluster, Join}
 import com.github.potamois.potamoi.commons.PotaConfig.RichConfig
 import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.concurrent.Eventually.eventually
@@ -23,7 +24,7 @@ import scala.language.postfixOps
  */
 trait STAkkaClusterMockSpec extends STAkkaSpec {
 
-  val nodeAkkaName = "potamoi-mock"
+  def actorSystemName: String = getClass.getName.split('.').last
 
   /**
    * ActorSystem creation behavior and remote canonical port.
@@ -33,15 +34,23 @@ trait STAkkaClusterMockSpec extends STAkkaSpec {
   /**
    * Launch a cluster consisting of several specified ActorSystem.
    */
-  def launchCluster[T](nodes: Role[T]*): Map[Role[T], ActorSystem[T]] = {
+  def launchCluster[T](nodes: Role[T]*)
+                      (implicit observerSystem: ActorSystem[_]): Map[Role[T], ActorSystem[T]] = {
     val seedPorts = nodes.map(_.port)
-    nodes.map { node =>
-      node -> ActorSystem(node.guardian, nodeAkkaName, genClusterConfig(node.port, seedPorts))
+    val cluster = nodes.map { node =>
+      node -> ActorSystem(node.guardian, actorSystemName, genClusterConfig(node.port, seedPorts))
     }.toMap
+    if (shouldObserverJoinCluster) Cluster(observerSystem).manager ! Join(cluster.head._2.address)
+    cluster
   }
 
+  private def shouldObserverJoinCluster(implicit observerSystem: ActorSystem[_]): Boolean =
+    observerSystem.settings.config.hasPath("akka.actor.provider") &&
+    observerSystem.settings.config.getString("akka.actor.provider") == "cluster"
+
+
   private def genClusterConfig(port: Int, seedPorts: Seq[Int]): Config = {
-    val seedContent = seedPorts.map(p => s""""akka://$nodeAkkaName@127.0.0.1:$p"""".stripMargin).mkString(",")
+    val seedContent = seedPorts.map(p => s""""akka://$actorSystemName@127.0.0.1:$p"""".stripMargin).mkString(",")
     ConfigFactory.parseString(
       s"""akka {
          |  actor.provider = "cluster"
@@ -64,18 +73,23 @@ trait STAkkaClusterMockSpec extends STAkkaSpec {
   /**
    * Blocking unit all ActorSystem is UP.
    */
-  def waitAllNodeUp(systems: Seq[ActorSystem[_]], waitTimeout: FiniteDuration = 60.seconds): Unit = {
+  def waitAllNodeUp(systems: Seq[ActorSystem[_]], waitTimeout: FiniteDuration = 60.seconds)
+                   (implicit originSystem: ActorSystem[_]): Unit = {
     systems.map(Cluster(_))
       .foreach(cluster =>
         eventually(timeout(waitTimeout), interval(500 millis)) {
           cluster.selfMember.status shouldBe MemberStatus.Up
         }
       )
+    if (shouldObserverJoinCluster)
+      eventually(timeout(waitTimeout), interval(500 millis)) {
+        Cluster(originSystem).selfMember.status shouldBe MemberStatus.Up
+      }
     log.info("[@potamoi-mock] all nodes are UP")
   }
 
 
-  implicit class ClusterNodesImplicits[T](clusterNodes: Map[Role[T], ActorSystem[T]]) {
+  implicit class ClusterNodesImplicits[T](clusterNodes: Map[Role[T], ActorSystem[T]])(implicit observerSystem: ActorSystem[_]) {
 
     // Blocking unit all ActorSystem is UP.
     def waitAllUp(timeout: FiniteDuration): Map[Role[T], ActorSystem[T]] = {
@@ -84,6 +98,14 @@ trait STAkkaClusterMockSpec extends STAkkaSpec {
     }
     def waitAllUp: Map[Role[T], ActorSystem[T]] = waitAllUp(60.seconds)
 
+    // println all members
+    def printMembers: Map[Role[T], ActorSystem[T]] = {
+      val observer = if (shouldObserverJoinCluster) observerSystem else clusterNodes.head._2
+      log.info(
+        s"""[@potamoi-mock] all members:
+           |${Cluster(observer).state.members.map("\t" + _.toString()).mkString("\n")}""".stripMargin)
+      clusterNodes
+    }
     // Shutdown all ActorSystem
     def shutdown(): Unit = {
       clusterNodes.values.foreach(_.terminate())
--- potamoi-akka-toolkit/src/test/scala/com/github/potamois/potamoi/akka/testkit/package.scala
+++ potamoi-akka-toolkit/src/test/scala/com/github/potamois/potamoi/akka/testkit/package.scala
@@ -1,11 +1,17 @@
 package com.github.potamois.potamoi.akka
 
-import com.github.potamois.potamoi.commons.PotaConfig
-import com.typesafe.config.Config
+import com.github.potamois.potamoi.commons.PotaConfig.RichConfig
+import com.typesafe.config.{Config, ConfigFactory}
 
 package object testkit {
 
   // default akka config for scalatest
-  val defaultConfig: Config = PotaConfig.root
+  val defaultConfig: Config = ConfigFactory.load().ensurePotamoi
+
+  // default akka cluster config for scalatest
+  val defaultClusterConfig: Config =
+    ConfigFactory.parseString(""" akka.actor.provider = "cluster" """.stripMargin)
+      .withFallback(ConfigFactory.load())
+      .ensurePotamoi
 
 }
