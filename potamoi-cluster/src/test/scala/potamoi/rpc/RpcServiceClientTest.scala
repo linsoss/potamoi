5112b4cb616d27f8dee4d896dc2e7e9ba70c711d
Al-assad <assad.dev@outlook.com>
2023-01-20 00:10:43 +0800

    kryo serialization tests for scala3 union type

--- potamoi-cluster/src/test/scala/potamoi/rpc/RpcServiceClientTest.scala
+++ potamoi-cluster/src/test/scala/potamoi/rpc/RpcServiceClientTest.scala
@@ -15,16 +15,23 @@ sealed trait BotProto
 object BotEntity extends EntityType[BotProto]("test")
 
 object BotProto:
-  case class Echo(msg: String, replier: Replier[String]) extends BotProto
-  case class Touch(msg: String)                          extends BotProto
+  case class Echo(msg: String, replier: Replier[String])                                extends BotProto
+  case class Touch(msg: String)                                                         extends BotProto
+  case class UnionType(sign: Int, replier: Replier[Int | String])                       extends BotProto // union type serialization test
+  case class UnionTypeEither(sign: Int, replier: Replier[Either[Int | String, String]]) extends BotProto // union type serialization test
 
 /**
  * Server
  */
 object BotRpcService extends RpcService[BotProto](BotEntity):
   def handleMessage(message: BotProto): URIO[Sharding, Unit] = message match {
-    case BotProto.Echo(msg, replier) => replier.reply("echo: " + msg)
-    case BotProto.Touch(msg)         => Console.printLine(s"be touch: $msg").ignore
+    case BotProto.Echo(msg, replier)             => replier.reply("echo: " + msg)
+    case BotProto.Touch(msg)                     => Console.printLine(s"be touch: $msg").ignore
+    case BotProto.UnionType(sign, replier)       => if sign > 10 then replier.reply(2333) else replier.reply("boom")
+    case BotProto.UnionTypeEither(sign, replier) =>
+      if sign < 0 then replier.reply(Right("pass"))
+      else if sign < 10 then replier.reply(Left(2333))
+      else replier.reply(Left("boom"))
   }
 
 object BotRpcServiceApp extends ZIOAppDefault:
@@ -46,8 +53,12 @@ object BotRpcClientApp extends ZIOAppDefault:
     for {
       bot       <- Rpc(BotEntity)
       echoReply <- bot.ask(BotProto.Echo("hello", _))
-      _         <- bot.tell(BotProto.Touch("hi"))
       _         <- Console.printLine(echoReply)
+      _         <- bot.tell(BotProto.Touch("hi"))
+      _         <- bot.ask(BotProto.UnionType(1, _)).debug
+      _         <- bot.ask(BotProto.UnionTypeEither(-2, _)).debug
+      _         <- bot.ask(BotProto.UnionTypeEither(5, _)).debug
+      _         <- bot.ask(BotProto.UnionTypeEither(10, _)).debug
     } yield ()
 
   val run = effect.provide(ShardingConf.test.copy(selfPort = 54322).asLayer, Shardings.test)
