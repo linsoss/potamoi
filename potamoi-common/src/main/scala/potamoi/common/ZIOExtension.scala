89240b21657d77e26971cb16a3797a6bd9382faf
Al-assad <assad.dev@outlook.com>
2023-02-03 11:09:15 +0800

    update

--- potamoi-common/src/main/scala/potamoi/common/ZIOExtension.scala
+++ potamoi-common/src/main/scala/potamoi/common/ZIOExtension.scala
@@ -45,9 +45,9 @@ object ZIOExtension {
 
     inline def debugPrettyWithTag(tag: String): ZIO[R, E, A] =
       zio
-        .tap(value => ZIO.succeed(println(s"<$tag> ${toPrettyString(value)}")))
+        .tap(value => ZIO.succeed(println(s"$tag: ${toPrettyString(value)}")))
         .tapErrorCause { case cause =>
-          ZIO.succeed(println(s"[$tag] <FAIL> ${cause.prettyPrint}"))
+          ZIO.succeed(println(s"<FAIL> $tag: ${cause.prettyPrint}"))
         }
 
     inline def repeatWhileWithSpaced(f: A => Boolean, spaced: Duration): ZIO[R, E, A] =
